bb134fb909993d27ba996edb0c06d26ece9740eb
Haknazar <haks15122006@gmail.com>
2026-03-05 08:18:36 +0500

    Обновлены файлы

--- Practice1/python-basics/basics.py
+++ Practice1/python-basics/basics.py
@@ -1,2 +1,13 @@
-a = 5
-print(a)
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+def squares_generator(start, end):
+    for i in range(start, end+1):
+        squared = i**2
+        yield squared
+
+gen = squares_generator(a, b)
+
+for num in gen:
+    print(num)
